3f3eb4f11c217b507e5eaf4a2c1582058c36b0fc
Mboutchouang <mboutchouangalex+github@gmail.com>
2022-12-13 17:24:43 +0100

    define Route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Public routes
+
+// List articles
+Route::get('articles', 'App\Http\Controllers\ArticleController@index');
+
+// List single article
+Route::get('articles/{id}', 'App\Http\Controllers\ArticleController@show');
+
+// Create new article
+Route::post('articles', 'App\Http\Controllers\ArticleController@store');
+
+// update an article
+Route::put('articles/{id}', 'App\Http\Controllers\ArticleController@update');
+
+// delete an article
+Route::delete('articles/{id}', 'App\Http\Controllers\ArticleController@destroy');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });

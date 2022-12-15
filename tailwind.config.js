2642d96a800856bec74f3629776da803d20b7946
Mboutchouang <mboutchouangalex+github@gmail.com>
2022-12-15 17:09:28 +0100

    configure tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./resources/**/*.blade.php",
@@ -6,6 +9,18 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      green: colors.green,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red
+    },
     extend: {},
   },
   plugins: [],
